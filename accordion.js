1360cc41f829195a65c5fe500ef094c297dcb638
conorjohnston <cnrswim@yahoo.com>
2019-10-19 21:39:50 -0400

    I'm an idiot and forgot to link my css until now

--- accordion.js
+++ accordion.js
@@ -5,12 +5,10 @@ var i;
 
 for (i = 0; i < list.length; i++) {
     list[i].innerHTML.addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-    //   var newL = document.querySelector('.newL')
-      all[i].classList.toggle("active");
+      
+      this.classList.toggle("active");
+
       var panel = this.nextElementSibling;
-      /* Toggle between hiding and showing the active panel */
       if (panel.style.display === "block") {
         panel.style.display = "none";
       } else {
